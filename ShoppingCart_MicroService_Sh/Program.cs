5407354086f38a687da0cfb7d328b5c642c4dbf3
mfrieman <mfrieman@hotmail.com>
2015-07-11 14:32:29 -0400

    Initial Commit

---
+++ ShoppingCart_MicroService_Sh/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart_MicroService_Sh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
